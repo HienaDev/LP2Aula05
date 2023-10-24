b93f8b3e75e91d5d313bfb9bb19c45cbed90231a
HienaDev <ant.rodrigues1999@gmail.com>
2023-10-24 15:35:51 +0100

    Added specific exceptions

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -14,6 +14,14 @@ namespace ExceptionTest
                 i = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine($"Número inserido: {i}");
             }
+            catch(FormatException)
+            {
+                Console.WriteLine("Tem que inserir um número inteiro");
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("Tem que ser menor que " + int.MaxValue);
+            }
             catch(Exception e)
             {
                 Console.Write("Ocorreu o seguinte problema: " + e.Message);    
