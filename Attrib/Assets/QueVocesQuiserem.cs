318817d1c4000afc433fc02a4b330bbabf2cb064
HienaDev <ant.rodrigues1999@gmail.com>
2023-10-24 14:53:23 +0100

    Created script

---
+++ Attrib/Assets/QueVocesQuiserem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QueVocesQuiserem : MonoBehaviour
+{
+
+    [SerializeField, Range(0f, 100f)] private int QualquerCoisa;
+
+}
